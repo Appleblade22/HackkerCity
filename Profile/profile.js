4b5ce1cf380fd34e01ae5c75c52b5f2d5f0464d4
APPLE <appleblade22@gmail.com>
2022-03-09 16:51:41 +0530

    DOne

---
+++ Profile/profile.js
@@ -0,0 +1,5 @@
+let loadMore = document.querySelector('#load-more');
+let submissionsCard = document.querySelector('#submissions-card-body');
+loadMore.addEventListener('click', () => {
+    submissionsCard.innerHTML += `<a href="">problem y</a> <i> - x days ago</i> <br>`;
+})
