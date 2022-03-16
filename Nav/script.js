21aa6e913bb10be8cbb55054d6a607c5bfb29060
MeetJain20 <meetjainn2002@gmail.com>
2022-03-16 16:04:11 +0530

    m

--- Nav/script.js
+++ Nav/script.js
@@ -3,22 +3,22 @@ const searchBtn = document.querySelector(".search-icon");
 const cancelBtn = document.querySelector(".cancel-icon");
 const items = document.querySelector(".nav-items");
 const form = document.querySelector("form");
-menuBtn.onclick = ()=>{
+menuBtn.onclick = () => {
   items.classList.add("active");
   menuBtn.classList.add("hide");
   searchBtn.classList.add("hide");
   cancelBtn.classList.add("show");
-}
-cancelBtn.onclick = ()=>{
+};
+cancelBtn.onclick = () => {
   items.classList.remove("active");
   menuBtn.classList.remove("hide");
   searchBtn.classList.remove("hide");
   cancelBtn.classList.remove("show");
   form.classList.remove("active");
   cancelBtn.style.color = "#ff3d00";
-}
-searchBtn.onclick = ()=>{
+};
+searchBtn.onclick = () => {
   form.classList.add("active");
   searchBtn.classList.add("hide");
   cancelBtn.classList.add("show");
-}
+};
