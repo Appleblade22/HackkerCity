d03a838bd884b3384218b07125e0c9895ac1f0f4
MeetJain20 <meetjainn2002@gmail.com>
2022-03-17 19:26:43 +0530

    m

--- Nav/script.js
+++ Nav/script.js
@@ -27,13 +27,15 @@ searchBtn.onclick = () => {
   cancelBtn.classList.add("show");
 };
 
-// window.onscroll = function() {myFunction()};
-// var navbar = document.getElementById("navbar");
-// var sticky = navbar.offsetTop;
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
+window.onscroll = function () {
+  myFunction();
+};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
