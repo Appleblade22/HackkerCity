e69e59654970652335686c7e74ac4b71426503c0
MeetJain20 <meetjainn2002@gmail.com>
2022-03-17 11:28:33 +0530

    f

--- Nav/script.js
+++ Nav/script.js
@@ -3,26 +3,37 @@ const searchBtn = document.querySelector(".search-icon");
 const cancelBtn = document.querySelector(".cancel-icon");
 const items = document.querySelector(".nav-items");
 const form = document.querySelector("form");
-menuBtn.onclick = ()=>{
-  document.body.classList.toggle('lock-scroll');
+menuBtn.onclick = () => {
+  document.body.classList.toggle("lock-scroll");
   items.classList.add("active");
   menuBtn.classList.add("hide");
   searchBtn.classList.add("hide");
   cancelBtn.classList.add("show");
-}
+};
 
-cancelBtn.onclick = ()=>{
-  document.body.classList.toggle('lock-scroll');
+cancelBtn.onclick = () => {
+  document.body.classList.toggle("lock-scroll");
   items.classList.remove("active");
   menuBtn.classList.remove("hide");
   searchBtn.classList.remove("hide");
   cancelBtn.classList.remove("show");
   form.classList.remove("active");
   cancelBtn.style.color = "#ff3d00";
-}
-searchBtn.onclick = ()=>{
-  document.body.classList.toggle('lock-scroll');
+};
+searchBtn.onclick = () => {
+  document.body.classList.toggle("lock-scroll");
   form.classList.add("active");
   searchBtn.classList.add("hide");
   cancelBtn.classList.add("show");
-}
+};
+
+// window.onscroll = function() {myFunction()};
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
