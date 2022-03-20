4f22b7d4bd191de861e66a7ff7051e712bfc8320
MeetJain20 <meetjainn2002@gmail.com>
2022-03-20 21:26:34 +0530

    m

---
+++ Skills/script.js
@@ -0,0 +1,12 @@
+function insertTest() {
+  const insertCard = document.querySelector(".row");
+  insertCard.innerHTML += `<div class="card">
+      <div class="card-body">
+        <p class="card-text">
+          <h4>Test</h4>
+        </p>
+      </div>
+    </div>`;
+}
+
+//insertTest();
