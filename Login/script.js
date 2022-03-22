eff13e67be0a7eaabfdaedc16b9e3fb4fab94169
APPLE <appleblade22@gmail.com>
2022-03-22 20:57:38 +0530

    Left

--- Login/script.js
+++ Login/script.js
@@ -36,27 +36,43 @@ function login(){
     window.location.href = "http://www.w3schools.com";
 }
 
-function signup(){
+const signup_btn = document.querySelector('#signup_form');
+signup_btn.addEventListener('submit', (e) => {
+    console.log("hello");
+    e.preventDefault();
     const form = document.getElementById("Signup");
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const cpassword = document.getElementById("cpassword").value;
     const username = document.getElementById("signupUsername").value;
+    let flag = 1;
     if(email === "" || password === "" || cpassword === "" || username === ""){
         setFormMessage(form, "error", "Please fill all the fields");
-        return;
+        flag = 0;
     }
     if(!ValidateEmail(email)){
         setFormMessage(form, "error", "Please enter a valid email");
-        return;
+        flag = 0;
     }
     if(password !== cpassword){
         setFormMessage(form, "error", "Passwords do not match");
-        return;
+        flag = 0;
     }
     //Do your Fetch operation with firebase
-    window.location.href = "http://www.w3schools.com";
-}
+    //Sign up User
+    console.log(flag);
+    if(flag){
+    createUserWithEmailAndPassword(auth, email, password).then(function(user){
+        setFormMessage(form, "success", "Account created successfully");
+        user.updateProfile({
+            displayName: username
+        });
+    }).catch(function(error) {
+        setFormMessage(form, "error", error.message);
+    });
+    }
+});   
+
 
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".form_input").forEach(inputElement => {
