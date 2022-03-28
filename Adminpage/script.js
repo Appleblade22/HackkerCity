9401a7e0c43ee333dba63c88cc2c6aa4cf2cbe1a
MeetJain20 <meetjainn2002@gmail.com>
2022-03-29 00:34:21 +0530

    m

--- Adminpage/script.js
+++ Adminpage/script.js
@@ -1,6 +1,44 @@
-const tablecontent = document.querySelector(".user");
+const user = document.querySelector(".user");
+const block = document.querySelector(".block");
+const deleteuser = document.querySelector(".delete");
 const hiddentable = document.querySelector(".submission-container");
 
-tablecontent.addEventListener("click", () => {
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+const headingcls = document.querySelector(".headingcls");
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+user.addEventListener("click", () => {
   hiddentable.classList.toggle("hidden");
+});
+
+block.addEventListener("click", () => {
+  hiddentable.classList.add("hidden");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  headingcls.textContent =
+    "The User Account will be temporarily blocked and cannot be able to access the website";
+});
+
+deleteuser.addEventListener("click", () => {
+  hiddentable.classList.add("hidden");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  headingcls.textContent =
+    "The User Account will be permanently deleted. Please click confirm to continue";
 });
