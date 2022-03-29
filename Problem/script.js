9138be3578d26fff1f65851d57c0ad74d47b96be
abp2021 <ajaybabu.p20@iiits.in>
2022-03-29 22:56:58 +0530

    done

--- Problem/script.js
+++ Problem/script.js
@@ -1,5 +1,4 @@
-// const division = document.querySelector(".constraint-ques");
-// document.addEventListener("click", () => {
-//   division.textContent =
-//     "awfijqhfijiqpfnjkqnfnqknfnqfnqkoenfpiqenfpikqenfpqienfkpqenfpkqnefnqefnqenfqeifkqoenfqnkefonqenfqnefokenfqnflqnefkqenfkqenfqnefkqnefklqneknqefnjwbrigqbj3orjoqbo3rbjojk2brkq3brkpqbfrbq3rojq3bripq3kb rlkqbrobqkrbkpqbriqpk3brqb3r0iq3br3";
-// });
+
+
+
+
