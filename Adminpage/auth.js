e6eef6deb802e7871a87fc73e589364650a850ce
APPLE <appleblade22@gmail.com>
2022-03-29 22:17:18 +0530

    Done

--- Adminpage/auth.js
+++ Adminpage/auth.js
@@ -1,20 +1,10 @@
 // Importing all modules
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js";
-
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-analytics.js";
 import {
   getDatabase,
-  set,
   ref,
-  update,
+  onValue,
 } from "https://www.gstatic.com/firebasejs/9.6.9/firebase-database.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  sendPasswordResetEmail,
-} from "https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js";
 
 //------------------------------------------
 
@@ -32,41 +22,58 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getDatabase();
 
-function SelectAllData() {
-  firebase
-    .database()
-    .ref("users")
-    .on("value", function (AllRecords) {
-      AllRecords.forEach(function (CurrentRecord) {
-        var username = CurrentRecord.val().username;
-        var email = CurrentRecord.val().email;
-        var lastlogin = CurrentRecord.val().last_login;
-        AddItemsToTable(email, username, lastlogin);
-        console.log(CurrentRecord);
-      });
+//Retrieve data from firebase
+function RetrieveData() {
+  const data = ref(db, "/users");
+  onValue(data, (snapshot) => {
+    snapshot.forEach((child) => {
+      child.val().email;
+      child.val().username;
+
+      // this.intVal.push(child.val());
+      // console.log("intVal", this.intVal);
     });
+
+  });
 }
+RetrieveData();
 
-document.addEventListene;
+// function SelectAllData() {
+//   firebase
+//     .database()
+//     .ref("users")
+//     .on("value", function (AllRecords) {
+//       AllRecords.forEach(function (CurrentRecord) {
+//         var username = CurrentRecord.val().username;
+//         var email = CurrentRecord.val().email;
+//         var lastlogin = CurrentRecord.val().last_login;
+//         AddItemsToTable(email, username, lastlogin);
+//         console.log(CurrentRecord);
+//       });
+//     });
+// }
 
-var stdNo = 0;
-function AddItemsToTable(email, username, lastlogin) {
-  var tbody = document.getElementById("tbody1");
-  var trow = document.createElement("tr");
-  var td1 = document.createElement("td");
-  var td2 = document.createElement("td");
-  var td3 = document.createElement("td");
-  var td4 = document.createElement("td");
-  td1.innerHTML = ++stdNo;
-  td2.innerHTML = email;
-  td3.innerHTML = username;
-  td4.innerHTML = lastlogin;
-  trow.appendChild(td1);
-  trow.appendChild(td2);
-  trow.appendChild(td3);
-  trow.appendChild(td4);
-  tbody.appendChild(trow);
-}
+// document.addEventListene;
 
-SelectAllData();
+// var stdNo = 0;
+// function AddItemsToTable(email, username, lastlogin) {
+//   var tbody = document.getElementById("tbody1");
+//   var trow = document.createElement("tr");
+//   var td1 = document.createElement("td");
+//   var td2 = document.createElement("td");
+//   var td3 = document.createElement("td");
+//   var td4 = document.createElement("td");
+//   td1.innerHTML = ++stdNo;
+//   td2.innerHTML = email;
+//   td3.innerHTML = username;
+//   td4.innerHTML = lastlogin;
+//   trow.appendChild(td1);
+//   trow.appendChild(td2);
+//   trow.appendChild(td3);
+//   trow.appendChild(td4);
+//   tbody.appendChild(trow);
+// }
+
+// SelectAllData();
