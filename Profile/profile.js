6403e4cf8918f711cfc94b101cdaccc3e00ac6d5
APPLE <appleblade22@gmail.com>
2022-03-29 16:26:27 +0530

    Done

--- Profile/profile.js
+++ Profile/profile.js
@@ -3,3 +3,20 @@ let submissionsCard = document.querySelector('#submissions-card-body');
 loadMore.addEventListener('click', () => {
     submissionsCard.innerHTML += `<a href="">problem y</a> <i> - x days ago</i> <br>`;
 })
+
+const logout = document.getElementById("logout");
+if (logout) {
+    logout.addEventListener("click", (event) => {
+        event.preventDefault();
+        localStorage.removeItem("userData");
+        window.location.href = "../Dashboard/index.html";
+    });
+}
+const log = document.getElementById("log");
+if (log) {
+    log.addEventListener("click", (event) => {
+        event.preventDefault();
+        localStorage.removeItem("userData");
+        window.location.href = "../Dashboard/index.html";
+    });
+}
