6ace175ad97f4ac975b20ab731f72f98359b7b38
abp2021 <ajaybabu.p20@iiits.in>
2022-03-29 22:58:38 +0530

    done

--- Skills/script.js
+++ Skills/script.js
@@ -10,28 +10,3 @@ function insertTest() {
       </div>
     </div>`;
 }
-<<<<<<< HEAD
-
-//Logout
-const logout = document.getElementById("logout");
-if (logout) {
-  logout.addEventListener("click", (event) => {
-    event.preventDefault();
-    localStorage.removeItem("userData");
-    window.location.href = "../Dashboard/index.html";
-  });
-}
-const log = document.getElementById("log");
-if (log) {
-  log.addEventListener("click", (event) => {
-    event.preventDefault();
-    localStorage.removeItem("userData");
-    window.location.href = "../Dashboard/index.html";
-  });
-}
-
-
-
-// insertTest()
-=======
->>>>>>> d5a1bd708d374e162e1bab17530d628786f180cd
