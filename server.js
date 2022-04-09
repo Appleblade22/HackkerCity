88f3b4a93e93f7a902372789ec2fccf2fe6836ba
APPLE <appleblade22@gmail.com>
2022-04-09 10:24:57 +0530

    done

--- server.js
+++ server.js
@@ -2,16 +2,24 @@ const { response } = require('express');
 const express = require('express');
 const { render } = require('express/lib/response')
 const app = express();
+const mongoose = require('mongoose');
 const bodyparser = require('body-parser')
 app.use(bodyparser.urlencoded({ extended: true }));
 let path = require('path');
-const dburi = "mongodb + srv://kali:kali@data.vcmov.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const dbuser = "mongodb+srv://kali:kali@data.vcmov.mongodb.net/User?retryWrites=true&w=majority";
 
+mongoose.connect(dbuser, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(3000, 'localhost', () => {
+            console.log("Server is running")
+        });
+    })
+    .catch(() => {
+        console.log("Connection failed");
+    });
 
 app.use(express.static(__dirname));
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/Dashboard/index.html")
 })
-app.listen(3000, 'localhost', () => {
-    console.log("Server is running")
-})
+
