11b102d2880171d962d0a316341cbc542621781d
APPLE <appleblade22@gmail.com>
2022-04-27 11:06:18 +0530

    Default code done

--- editor/lib/JS/editor.js
+++ editor/lib/JS/editor.js
@@ -1,39 +1,30 @@
 //Retrieve Elements
 const executeCodebtn = document.querySelector(".editor__submit");
 const resetCodebtn = document.querySelector(".editor__reset");
-const defaultCode = "// Hello my friends" + "\n";
+//make a default code for C, c++, java, python
+let dcode = "";
+const defaultCode = {
+
+  'C++': `#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}`,
+
+  "C": `#include <stdio.h>
+int main() {
+    printf("Hello World!");
+    return 0;
+}`,
+  "python": `print("Hello World!")`,
+  "javascript": `console.log("Hello World!")`
+};
+
+
 
 //Setup ace
 let codeEditor = ace.edit("editorCode");
-// function setsesh(){
-//   fetch('/getlang', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify({
-//         url: window.location.href,
-//         })
-//     })
-//     .then(res => res.json())
-//     .then(data => {
-//       if(data.status){
-//         if(data.lang === "C++"){
-//           codeEditor.session.setMode("ace/mode/c_cpp");
-//         } else if(data.lang === "Java"){
-//           codeEditor.session.setMode("ace/mode/java");
-//         }
-//         else if(data.lang === "Python"){
-//           codeEditor.session.setMode("ace/mode/python");
-//         }
-//         else if(data.lang === "C"){
-//           codeEditor.session.setMode("ace/mode/c_cpp");
-//         }
-//       }
-//     }
-//   )
-
-// }
 let consoleMessages = [];
 let language = "C++";
 let editorlib = {
@@ -50,7 +41,7 @@ let editorlib = {
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log("lang: " + data.lang);
+        language = data.lang;
         if (data.status) {
           if (data.lang === "C++") {
             codeEditor.session.setMode("ace/mode/c_cpp");
@@ -65,6 +56,18 @@ let editorlib = {
             language = "C";
           }
         }
+        // codeEditor.setValue(defaultCode[language.toLowerCase]);
+
+        if (language.toLowerCase() === "c++") {
+          dcode = defaultCode["C++"];
+        } else if (language.toLowerCase() === "c") {
+          dcode = defaultCode["C"];
+        } else if (language.toLowerCase() === "python") {
+          dcode = defaultCode["python"];
+        } else if (language.toLowerCase() === "javascript") {
+          dcode = defaultCode["javascript"];
+        }
+        codeEditor.setValue(dcode);
       });
     codeEditor.setOptions({
       enableBasicAutocompletion: true,
@@ -77,7 +80,6 @@ let editorlib = {
       tabSize: 2,
       useSoftTabs: true,
     });
-    codeEditor.setValue(defaultCode);
   },
 };
 //Events
@@ -104,7 +106,7 @@ executeCodebtn.addEventListener("click", function () {
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         if (data.status) {
           editMessage.innerHTML =
             `<i class="bi bi-check-circle"></i>` + "ALL TEST CASES PASSED!";
@@ -134,7 +136,7 @@ executeCodebtn.addEventListener("click", function () {
 });
 
 resetCodebtn.addEventListener("click", function () {
-  codeEditor.setValue(defaultCode);
+  codeEditor.setValue(dcode);
 });
 
 editorlib.init();
